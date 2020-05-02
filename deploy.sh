aa6f14e8162e704e41376c6217bd0291e5a847d7
Wes Bonelli <wpb3hw@virginia.edu>
2020-05-02 16:05:17 -0400

    fix graylog root password sha2 var name

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 host="$1"
-graylog_password="$2"
+graylog_root_password_sha2="$2"
 compose="docker-compose -f docker-compose.yml"
 
 $compose down
@@ -10,5 +10,5 @@ git pull
 sudo chmod +x bootstrap.sh
 ./bootstrap.sh
 find .env -type f -exec sed -i "s/localhost/$host/g" {} \;
-find .env -type f -exec sed -i "s/GRAYLOG_ROOT_PASSWORD_SHA2=*\n/$graylog_password/g" {} \;
+find .env -type f -exec sed -i "s/GRAYLOG_ROOT_PASSWORD_SHA2=*\n/$graylog_root_password_sha2/g" {} \;
 $compose up -d
